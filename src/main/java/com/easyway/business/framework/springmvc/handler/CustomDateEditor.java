54060a5e6a6ca3f837461aa92f47f5b0d0fc898d
liuxl <ylulu2007@sina.com>
2019-08-27 10:05:07 +0800

    1

--- src/main/java/com/easyway/business/framework/springmvc/handler/CustomDateEditor.java
+++ src/main/java/com/easyway/business/framework/springmvc/handler/CustomDateEditor.java
@@ -21,52 +21,21 @@ public class CustomDateEditor extends PropertyEditorSupport {
 
 	private final int exactDateLength;
 
-	/**
-	 * Create a new CustomDateEditor instance, using the given DateFormat
-	 * for parsing and rendering.
-	 * <p>The "allowEmpty" parameter states if an empty String should
-	 * be allowed for parsing, i.e. get interpreted as null value.
-	 * Otherwise, an IllegalArgumentException gets thrown in that case.
-	 * @param dateFormat DateFormat to use for parsing and rendering
-	 * @param allowEmpty if empty strings should be allowed
-	 */
 	public CustomDateEditor(DateFormat dateFormat, boolean allowEmpty) {
 		this.dateFormat = dateFormat;
 		this.allowEmpty = allowEmpty;
 		this.exactDateLength = -1;
 	}
 
-	/**
-	 * Create a new CustomDateEditor instance, using the given DateFormat
-	 * for parsing and rendering.
-	 * <p>The "allowEmpty" parameter states if an empty String should
-	 * be allowed for parsing, i.e. get interpreted as null value.
-	 * Otherwise, an IllegalArgumentException gets thrown in that case.
-	 * <p>The "exactDateLength" parameter states that IllegalArgumentException gets
-	 * thrown if the String does not exactly match the length specified. This is useful
-	 * because SimpleDateFormat does not enforce strict parsing of the year part,
-	 * not even with {@code setLenient(false)}. Without an "exactDateLength"
-	 * specified, the "01/01/05" would get parsed to "01/01/0005". However, even
-	 * with an "exactDateLength" specified, prepended zeros in the day or month
-	 * part may still allow for a shorter year part, so consider this as just
-	 * one more assertion that gets you closer to the intended date format.
-	 * @param dateFormat DateFormat to use for parsing and rendering
-	 * @param allowEmpty if empty strings should be allowed
-	 * @param exactDateLength the exact expected length of the date String
-	 */
 	public CustomDateEditor(DateFormat dateFormat, boolean allowEmpty, int exactDateLength) {
 		this.dateFormat = dateFormat;
 		this.allowEmpty = allowEmpty;
 		this.exactDateLength = exactDateLength;
 	}
 
-	/**
-	 * Parse the Date from the given text, using the specified DateFormat.
-	 */
 	@Override
 	public void setAsText(String text) throws IllegalArgumentException {
 		if (this.allowEmpty && !StringUtils.hasText(text)) {
-			// Treat empty String as null value.
 			setValue(null);
 		}
 		else if (text != null && this.exactDateLength >= 0 && text.length() != this.exactDateLength) {
@@ -92,9 +61,6 @@ public class CustomDateEditor extends PropertyEditorSupport {
 		}
 	}
 
-	/**
-	 * Format the Date as String, using the specified DateFormat.
-	 */
 	@Override
 	public String getAsText() {
 		Date value = (Date) getValue();
