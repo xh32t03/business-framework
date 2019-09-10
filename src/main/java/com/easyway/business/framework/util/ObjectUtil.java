59b76a06ab14841b1aa7ef393cf9b1098cc45721
liuxl <ylulu2007@sina.com>
2019-09-10 12:59:20 +0800

    1

--- src/main/java/com/easyway/business/framework/util/ObjectUtil.java
+++ src/main/java/com/easyway/business/framework/util/ObjectUtil.java
@@ -43,23 +43,6 @@ public final class ObjectUtil {
     public static boolean isNotEmpty(Object obj) {
         return !isEmpty(obj);
     }
-    
-    /**
-     * 获取指定位数的随机字符串(包含小写字母、大写字母、数字，0<length)
-     * 
-     * @param length
-     * @return
-     */
-    public static String getRandomString(int length) {
-        // 随机字符串的随机字符库
-        String KeyString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuffer sb = new StringBuffer();
-        int len = KeyString.length();
-        for (int i = 0; i < length; i++) {
-            sb.append(KeyString.charAt((int) Math.round(Math.random() * (len - 1))));
-        }
-        return sb.toString();
-    }
 
     public static <A, O extends A> A[] addObjectToArray(A[] array, O obj) {
         Class<?> compType = Object.class;
