cf9776a92322f8380500ea43d52795cf60318ac3
liuxl <ylulu2007@sina.com>
2019-10-17 17:08:06 +0800

    1

--- src/main/java/com/easyway/business/framework/util/ConvertUtil.java
+++ src/main/java/com/easyway/business/framework/util/ConvertUtil.java
@@ -1,10 +1,15 @@
 package com.easyway.business.framework.util;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.cglib.beans.BeanMap;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ConvertUtil {
     /**
@@ -80,5 +85,39 @@ public class ConvertUtil {
             }
         }
         return list;
+    }
+    
+    /**
+     * @Description
+     *              <p>
+     *              获取到对象中属性为null的属性名
+     *              </P>
+     * @param source 要拷贝的对象
+     * @return
+     */
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<String>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null)
+                emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    /**
+     * @Description
+     *              <p>
+     *              拷贝非空对象属性值
+     *              </P>
+     * @param source 源对象
+     * @param target 目标对象
+     */
+    public static void copyProperties(Object source, Object target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
     }
 }
