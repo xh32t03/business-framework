980b36362d6d0d5b0374f5aead11aa9e115ffa31
liuxl <ylulu2007@sina.com>
2020-11-26 13:47:19 +0800

    1

--- src/main/java/com/easyway/business/framework/pojo/QueryPojo.java
+++ src/main/java/com/easyway/business/framework/pojo/QueryPojo.java
@@ -1,20 +1,26 @@
 package com.easyway.business.framework.pojo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-
+import java.util.Set;
 import com.easyway.business.framework.mybatis.query.ConditionQuery;
 import com.easyway.business.framework.mybatis.query.condition.Condition;
 import com.easyway.business.framework.mybatis.util.ConditionUtil;
 
 public class QueryPojo {
 
+    private Set<Condition> appendCondition = null;
     private Map<String, Object> queryParamMap = null;
 
     public ConditionQuery buildConditionQuery() {
         ConditionQuery query = newConditionQuery();
         query.addAll(toConditions());
+        if (this.appendCondition != null) {
+            query.addAll(new ArrayList<>(appendCondition));
+        }
         if (this.queryParamMap != null) {
             query.addAllParam(this.queryParamMap);
         }
@@ -34,6 +40,13 @@ public class QueryPojo {
             this.queryParamMap = new HashMap<String, Object>();
         }
         this.queryParamMap.put(key, value);
+    }
+    
+    public void appendCondition(Condition condition) {
+        if (this.appendCondition == null) {
+            this.appendCondition = new HashSet<Condition>();
+        }
+        this.appendCondition.add(condition);
     }
 
 }
