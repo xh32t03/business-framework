ca8f65e2fb23391e429f647afe57dd08155c12cb
liuxl <ylulu2007@sina.com>
2020-11-26 13:42:07 +0800

    1

--- src/main/java/com/easyway/business/framework/mybatis/query/condition/ConditionFactory.java
+++ src/main/java/com/easyway/business/framework/mybatis/query/condition/ConditionFactory.java
@@ -7,23 +7,44 @@ public class ConditionFactory {
         return new WithoutValueCondition(sql);
     }
 
-    public static Condition buildSingleValueCondition(String tableAlias, String field, String equal,
-            Object value) {
-        return new SingleValueCondition(tableAlias, field, equal, value);
+    public static Condition buildSingleValueCondition(String tableAlias, String column, String equal, Object value) {
+        return new SingleValueCondition(tableAlias, column, equal, value);
     }
 
-    public static Condition buildBetweenCondition(String column, Object beginValue,
+    public static Condition buildSingleValueCondition(String column, String equal, Object value) {
+        return new SingleValueCondition(column, equal, value);
+    }
+    
+    public static Condition buildBetweenCondition(String tableAlias, String column, Object beginValue,
             Object endValue) {
+        return new BetweenValueCondition(tableAlias, column, beginValue, endValue);
+    }
+
+    public static Condition buildBetweenCondition(String column, Object beginValue, Object endValue) {
         return new BetweenValueCondition(column, beginValue, endValue);
     }
-
-    public static Condition buildListCondition(String tableAlias, String column, String equal,
-            List<Object> list) {
+    
+    public static Condition buildListCondition(String tableAlias, String column, String equal, List<Object> list) {
         return new ListValueCondition(tableAlias, column, equal, list);
     }
 
-    public static Condition buildListCondition(String tableAlias, String column, String equal,
-            Object[] list) {
+    public static Condition buildListCondition(String tableAlias, String column, String equal, Object[] list) {
         return new ListValueCondition(tableAlias, column, equal, list);
+    }
+
+    public static Condition buildListCondition(String column, String equal, List<Object> list) {
+        return new ListValueCondition(column, equal, list);
+    }
+
+    public static Condition buildListCondition(String column, String equal, Object[] list) {
+        return new ListValueCondition(column, equal, list);
+    }
+    
+    public static Condition buildListCondition(String column, List<Object> list) {
+        return new ListValueCondition(column, list);
+    }
+
+    public static Condition buildListCondition(String column, Object[] list) {
+        return new ListValueCondition(column, list);
     }
 }
