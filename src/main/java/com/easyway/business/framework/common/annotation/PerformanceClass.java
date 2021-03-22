27ea5c3e887d6809ce4a0319c442d4dd5a7e75ff
liuxl <ylulu2007@sina.com>
2021-03-22 14:42:42 +0800

    1

--- src/main/java/com/easyway/business/framework/common/annotation/PerformanceClass.java
+++ src/main/java/com/easyway/business/framework/common/annotation/PerformanceClass.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  * 
  * @author xl.liu
  */
-@Target({ElementType.METHOD, ElementType.TYPE})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface PerformanceClass {
