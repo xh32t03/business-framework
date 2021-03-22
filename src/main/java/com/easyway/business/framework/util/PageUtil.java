f8e16bec52c9ae8b833386f276de95d3097fb9dd
liuxl <ylulu2007@sina.com>
2021-03-22 16:11:41 +0800

    1

---
+++ src/main/java/com/easyway/business/framework/util/PageUtil.java
@@ -0,0 +1,49 @@
+package com.easyway.business.framework.util;
+
+import java.util.List;
+
+public final class PageUtil {
+    /**
+     * 开始分页
+     * 
+     * @param list
+     * @param pageNum 页码
+     * @param pageSize 每页多少条数据
+     * @return
+     */
+    public static <T> List<T> startPage(List<T> list, Integer pageNum, Integer pageSize) {
+        if (list == null) {
+            return null;
+        }
+        if (list.size() == 0) {
+            return null;
+        }
+        
+        Integer count = list.size(); // 记录总数
+        Integer pageCount = 0; // 页数
+        if (count % pageSize == 0) {
+            pageCount = count / pageSize;
+        } else {
+            pageCount = count / pageSize + 1;
+        }
+
+        int fromIndex = 0; // 开始索引
+        int toIndex = 0; // 结束索引
+
+        if (pageNum != pageCount) {
+            fromIndex = (pageNum - 1) * pageSize;
+            toIndex = fromIndex + pageSize;
+        } else {
+            fromIndex = (pageNum - 1) * pageSize;
+            toIndex = count;
+        }
+
+        List<T> pageList = null;
+        try {
+            pageList = list.subList(fromIndex, toIndex);
+        } catch (Exception e) {
+        }
+        
+        return pageList;
+    }
+}
