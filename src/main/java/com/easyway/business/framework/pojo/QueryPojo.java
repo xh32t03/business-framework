f60d4d48622bb226fe073f0fa4d6ff8b3ef811aa
liuxl <ylulu2007@sina.com>
2021-03-22 14:16:30 +0800

    1

--- src/main/java/com/easyway/business/framework/pojo/QueryPojo.java
+++ src/main/java/com/easyway/business/framework/pojo/QueryPojo.java
@@ -1,5 +1,6 @@
 package com.easyway.business.framework.pojo;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,7 +10,9 @@ import java.util.Set;
 import com.easyway.business.framework.mybatis.query.ConditionQuery;
 import com.easyway.business.framework.mybatis.query.condition.Condition;
 import com.easyway.business.framework.mybatis.util.ConditionUtil;
+import com.easyway.business.framework.util.CollectionUtil;
 
+@SuppressWarnings("all")
 public class QueryPojo {
 
     private Set<Condition>      appendCondition = null;
@@ -24,6 +27,29 @@ public class QueryPojo {
         if (this.queryParamMap != null) {
             query.addAllParam(this.queryParamMap);
         }
+
+        try {
+            List<String> filterList = CollectionUtil.arrayToList(new String[] {"sortname", "sortorder", "pageSize", "pageNum",
+                    "dataList", "pages", "total", "appendCondition", "queryParamMap"});
+            Map<String, Field> fieldMap = getClassFields(this.getClass(), true);
+            for (String key : fieldMap.keySet()) {
+                Field field = fieldMap.get(key);
+                if (!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+                try {
+                    if (!filterList.contains(key)) {
+                        Object value = field.get(this);
+                        if (value != null) {
+                            appendQueryParam(key, value);
+                        }
+                    }
+                } catch (Exception e) {
+                }
+            }
+        } catch (Exception e) {
+        }
+
         return query;
     }
 
@@ -47,6 +73,30 @@ public class QueryPojo {
             this.appendCondition = new HashSet<Condition>();
         }
         this.appendCondition.add(condition);
+    }
+
+    public static Map<String, Field> getClassFields(Class clazz, boolean includeParentClass) {
+        Map<String, Field> map = new HashMap<String, Field>();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            map.put(field.getName(), field);
+        }
+        if (includeParentClass) {
+            getParentClassFields(map, clazz.getSuperclass());
+        }
+        return map;
+    }
+
+    private static Map<String, Field> getParentClassFields(Map<String, Field> map, Class clazz) {
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            map.put(field.getName(), field);
+        }
+        if (clazz.getSuperclass() == null) {
+            return map;
+        }
+        getParentClassFields(map, clazz.getSuperclass());
+        return map;
     }
 
 }
