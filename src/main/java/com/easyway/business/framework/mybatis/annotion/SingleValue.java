d7960968b9b5e9a2ec065a169ad201045735354a
liuxl <ylulu2007@sina.com>
2021-03-31 10:27:39 +0800

    1

--- src/main/java/com/easyway/business/framework/mybatis/annotion/SingleValue.java
+++ src/main/java/com/easyway/business/framework/mybatis/annotion/SingleValue.java
@@ -4,11 +4,11 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Documented
-@Target({ java.lang.annotation.ElementType.CONSTRUCTOR,
-		java.lang.annotation.ElementType.METHOD })
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 public @interface SingleValue {
